13ad5c05c4acf6e580f959c59d260ecc5edad9cd
Filipe Dias <mail@filipedias.me>
2017-08-30 17:50:38 +0100

    add parent class with shared functionality

---
+++ queues/queue.js
@@ -0,0 +1,21 @@
+'use strict';
+
+class Queue {
+
+
+  standardizeJob (jobName,job) {
+
+    let jobQueue = {
+      jobInfo : {
+        jobName,
+        attempts : 0
+      },
+      payload : job
+    };
+    return JSON.stringify(jobQueue);
+
+  }
+
+}
+
+module.exports = Queue;
